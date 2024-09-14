ca4f7b5f8c3df1388fa5c950cf00acb159bf1c71
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2024-09-14 14:40:58 +0200

    draws when i press the mouse

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-/****************************************************** */
+/************ INITIAL PARAMETERS *********************** */
 
 const container = document.getElementById('container');
 let gridSize = 960;
 createGrid(16);
-
-
+let isDrawing = false; 
 
 /****************************************************** */ 
+document.addEventListener("mouseup", ()=>{
+  isDrawing = false;
+})
+
 function resetGrid(){
   let blocks = document.querySelectorAll(".block");
   blocks.forEach(element => {
@@ -17,25 +20,36 @@ function resetGrid(){
 function createGrid(n){
   let grid = document.createElement("div");
   grid.setAttribute("id", "grid");
+  const newGridSize = gridSize/n;
+
   for (let i = 0; i < n*n; i++) {
     var div = document.createElement("div");
-    div.style.width = `${gridSize/n}px`;
-    div.style.height = `${gridSize/n}px`;
+    div.style.width = `${newGridSize}px`;
+    div.style.height = `${newGridSize}px`;
     div.setAttribute("class", "block");
     grid.appendChild(div);
   }
   container.append(grid);
-  grid.addEventListener("mouseover", (e) =>{
-    let target = e.target;
-    target.classList.add("block-hovered");
-  })
-  
+
+  let blocks = document.querySelectorAll(".block");
+  blocks.forEach(block => {
+    block.addEventListener("mousedown", (e)=>{
+      isDrawing=true;
+      e.target.classList.add("block-hovered");
+    })
+    block.addEventListener("mouseover", (e) =>{
+      if (isDrawing == true){
+         e.target.classList.add("block-hovered");
+      }
+    })
+  });
 }
 
 function modifyGrid(n){
   grid.remove();
   createGrid(n);
 }
+
 const resetButton = document.getElementById('grid-reset');
 resetButton.addEventListener('click', resetGrid)
 
