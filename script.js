3a2751e9974e9b6e87c3323c09499f653c857fc5
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2024-09-14 14:19:37 +0200

    modifiable grid size

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 /****************************************************** */
 
 const container = document.getElementById('container');
-const grid = document.getElementById('grid');
-const DEFAULT_COLOR = "rgb(221, 209, 209)";
+let gridSize = 960;
+createGrid(16);
 
-// create 16*16 divs
-for (let i = 0; i < 16*16; i++) {
-    var div = document.createElement("div");
-    div.setAttribute("class", "block");
-    grid.appendChild(div);
-}
-
-// Handle mouse over event to turn it black
-grid.addEventListener("mouseover", (e) =>{
-  let target = e.target;
-  target.classList.add("block-hovered");
-})
 
 
 /****************************************************** */ 
@@ -25,6 +13,29 @@ function resetGrid(){
     element.classList.remove("block-hovered");
   });
 }
+
+function createGrid(n){
+  let grid = document.createElement("div");
+  grid.setAttribute("id", "grid");
+  for (let i = 0; i < n*n; i++) {
+    var div = document.createElement("div");
+    div.style.width = `${gridSize/n}px`;
+    div.style.height = `${gridSize/n}px`;
+    div.setAttribute("class", "block");
+    grid.appendChild(div);
+  }
+  container.append(grid);
+  grid.addEventListener("mouseover", (e) =>{
+    let target = e.target;
+    target.classList.add("block-hovered");
+  })
+  
+}
+
+function modifyGrid(n){
+  grid.remove();
+  createGrid(n);
+}
 const resetButton = document.getElementById('grid-reset');
 resetButton.addEventListener('click', resetGrid)
 
@@ -33,5 +44,5 @@ const resizeButton = document.getElementById('grid-resize');
 resizeButton.addEventListener('click', () => {
   // Shows an alert that asks for how many squares do they want
   let newGridSize = prompt("Enter the size of the grid, between 1 and 100 inclusive");
-  createGrid(newGridSize);
+  modifyGrid(newGridSize);
 })
